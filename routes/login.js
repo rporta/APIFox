2f309952534d1d1e9d0be5ef46775cb177c5475e
Ramiro <ramiro.portas@gmail.com>
2019-03-26 17:19:06 -0300

    persistir f

--- routes/login.js
+++ routes/login.js
@@ -7,56 +7,74 @@ var logger, mssql;
 /*Request
 
 {
-  "msisdn": "string",
-  "pin": "string",
-  "user_id": 0,
-  "country_code": "string",
-  "client_request_id": "string"
+"msisdn": "string",
+"pin": "string",
+"user_id": 0,
+"country_code": "string",
+"client_request_id": "string"
 }
 
 Response
 
 {
-  "result": {
-    "login_status": 0,
-    "subscriber_status": 0,
-    "billing_status": 0,
-    "user_id": 0,
-    "msisdn": "string",
-    "carrier_id": 0,
-    "server_response_id": "string"
-  },
-  "status": {
-    "code": 0,
-    "description": "string"
-  }
+"result": {
+	"login_status": 0,
+	"subscriber_status": 0,
+	"billing_status": 0,
+	"user_id": 0,
+	"msisdn": "string",
+	"carrier_id": 0,
+	"server_response_id": "string"
+},
+"status": {
+"code": 0,
+"description": "string"
+}
 }*/
 
 router.post('/', function(req, res, next) {
-    logger = req.app.locals.logger;
-    mssql = req.app.locals.mssql;
+	logger = req.app.locals.logger;
+	mssql = req.app.locals.mssql;
 
 	var body = '';
-    var result = '';
-    req.on('data', function (data){ body += data; });
-    req.on('end', function() {
-        var data = JSON.parse(body);
-        //console.log(data);
-        logger.debug('Raw request: ' + body);
-        if (data.msisdn && data.pin && data.user_id && data.country_code){
-        	login(data, function(err, rs){
-        		if (!err){
-        			//aca preparar json de respuesta caso exitoso
-        			res.send({ status: 0});
-        		}else{
-        			//aca preparar json de respuesta con error
-        			res.status(400).send({ status: -1});
-        		}
-        	});
-        }else{
-        	res.status(400).send({ status: -1});
-        }
-    });
+	var result = '';
+	req.on('data', function (data){ body += data; });
+	req.on('end', function() {
+		var data = JSON.parse(body);
+		//console.log(data);
+		logger.debug('Raw request: ' + body);
+		if (data.msisdn && data.pin && data.user_id && data.country_code){
+			login(data, function(err, rs){
+
+				var response = {};
+				
+				if (!err){
+					//aca preparar json de respuesta caso exitoso
+					response['result'] = {};
+					response.result['login_status'] = 0;
+					response.result['subscriber_status'] = 0;
+					response.result['billing_status'] = 0;
+					response.result['msisdn'] = "string";
+					response.result['carrier_id'] = 0;
+					response.result['server_response_id'] = "string";
+					response['status'] = {};
+					response.status['code'] = 0;
+					response.status['description'] = "string";
+
+					res.send(response);
+				}else{
+					//aca preparar json de respuesta con error
+					response['result'] = {};
+					response['status'] = {};
+					response.status['code'] = 0;
+					response.status['description'] = "Invalid authorization header";
+					res.status(400).send(response);
+				}
+			});
+		}else{
+			res.status(400).send({ status: -1});
+		}
+	});
 });
 
 function login(data, cb){
@@ -69,7 +87,7 @@ function login(data, cb){
 			console.log(rs);
 			cb(null, rs);
 		}
-	)
+		)
 }
 
 
