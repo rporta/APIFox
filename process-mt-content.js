2d38278d4de81a12f91ba991ecdae0294b86d308
Ramiro <ramiro.portas@gmail.com>
2019-04-03 18:03:46 -0300

    se desarrolla proceso (para consutar en redis y procesar los request que se usaron para los metodos mt-content, bulk-mt-content) para el servicio forever

---
+++ process-mt-content.js
@@ -0,0 +1,85 @@
+var async = require('async');
+var redis = require('../libs/redis');
+var config = require('./config/config');
+var debug = config.debug;
+
+redis.setConfig(config);
+redis.setLogger(redisLogger);
+redis.init();
+
+/**
+ * Ramiro Portas : #ff
+ * (1) cada 60000ms = 1m ejecuto asyncResolveMtContent
+ */
+ setinterval(asyncResolveMtContent(null, (err, rs) => {
+ 	var response = {};			
+ 	if (!err){
+
+ 	}
+ 	else{
+
+ 	}
+ }), 60000);
+//#ff
+
+/**
+ * Ramiro Portas : Funcion que implementa async.waterfall
+ * @param  {[Regular Obj]}		data	[description] objeto regular que contiene parametros de tipo path y body del request
+ * @param  {[Function]}			cb		[description] funcion anonima que recibe 2 parametros : err, rs
+ * @return {[void]}						[description] quien resuelve es el callback
+ */
+ var asyncResolveProcessMtContent = (data, cb) => {
+
+	//vector de funciones
+	var ini = [
+	(cb) => {
+		debug
+		? logger.debug('asyncResolveProcessMtContent(): Execute process... 1 [CONSULO EN REDIS]') 
+		: null;
+		
+		//persisto request en redis
+		try{
+			var k, v;
+			k = "apifox-mt-content-" + data.body.content_id;
+			v = JSON.stringify(data.body)
+			redis.set(k, v);
+			cb(null, data);
+		}
+		catch(e){
+			cb(data, null);
+		}
+	},
+	// (data, cb) => {
+	// 	debug
+	// 	? logger.debug('asyncResolveProcessMtContent(): Execute process... 2') 
+	// 	: null;
+
+	// 	cb(null, data);
+	// },
+	// (data, cb) => {
+	// 	debug
+	// 	? logger.debug('asyncResolveProcessMtContent(): Execute process... 3') 
+	// 	: null;
+
+	// 	cb(null, data);
+	// },
+	];
+
+	//funcion final
+	var final = (err, rs) => {
+		debug
+		? logger.debug('asyncResolveProcessMtContent(): Execute final... ')
+		: null;
+
+		err
+		? (() => {
+			logger.debug('asyncResolveProcessMtContent(): Error final... ');
+			console.log(err);
+			cb(err, rs);
+		})()
+		: cb(err, rs);
+	};
+
+	//registro vector funciones, funcion final
+	async.waterfall(ini, final);	
+}
